b025a479a2c24eba140bca850061123f172f9d1a
jeje rome <jege.roome@gmail.com>
2023-03-25 19:05:55 +0100

    initial commit & configuration

---
+++ src/test/java/fr/eql/ai113/LesVentsDalizes/LesVentsDalizesApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.eql.ai113.LesVentsDalizes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LesVentsDalizesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
